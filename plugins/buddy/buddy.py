c9847841e2c2f1ab1e66025a3719ea807b26a15b
Tim Jenks <tim.jenks@eegeo.com>
2015-09-02 18:20:17 +0100

    Add john.bell to buddy round robin

--- plugins/buddy/buddy.py
+++ plugins/buddy/buddy.py
@@ -4,7 +4,7 @@ import random
 crontable = []
 outputs = []
 
-buddies = ["tim.jenks", "mark.simpson", "scott", "malcolm.brown", "ian.hutchinson", "jonty", "oliver.norton", "vimarsh.raina", "paul.harris"]
+buddies = ["tim.jenks", "mark.simpson", "scott", "malcolm.brown", "ian.hutchinson", "jonty", "oliver.norton", "vimarsh.raina", "paul.harris", "john.bell"]
 cursor = -1
 
 def usage():
